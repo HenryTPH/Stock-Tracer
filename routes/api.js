b794c0a1867f75c31d741dd444210b54181d1256
Henry Tu <hungtp2912@gmail.com>
2024-04-13 23:09:17 -0600

    Finish all functionality

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,9 @@
 'use strict';
 
-var expect = require('chai').expect;
+var expect = require('chai').expect
 let mongodb = require('mongodb')
 let mongoose = require('mongoose')
-
+let httpRequest = require("xmlhttprequest").XMLHttpRequest
 
 
 module.exports = function (app) {
@@ -24,7 +24,7 @@ module.exports = function (app) {
     .get(function (req, res){
       let response = {}
       response['data'] = {}
-
+      
       // Determine the number of stock
       let isTwoStock = false
 
@@ -37,52 +37,100 @@ module.exports = function (app) {
       let findOrUpdateStock = (stockName, documentUpdate, next) => {
         Stock.findOneAndUpdate({name: stockName}, 
                                 documentUpdate, 
-                                {new: true, upsert: true}, 
-                                (err, stockDocument) => {
-                                  if (err) {
-                                    console.log(err)
-                                  } else if (!err && stockDocument) {
-                                    if(!isTwoStock) {
-                                      return next(stockDocument, getOneStock)
-                                    }
+                                {new: true, upsert: true}).then(result => {
+                                  if (!isTwoStock && result) {
+                                    return next(result, getOneStock)
+                                  } else if (isTwoStock && result){
+                                    return next(result, getTwoStock)
                                   }
-        })
+                                })
       }
 
       // Like of stock
-      let likeStock = (stockName, next) => {
-
+      let likeStock = async (stockName, next) => {
+        await Stock.findOne({name: stockName}).then(stock => {
+          console.log('Like stock')
+          console.log(req.ip)
+          if(stock && stock['ips'] && stock['ips'].includes(req.ip)) {
+            return res.send('Error: Only 1 like per IP allowed')
+          } else {
+            let docUpdate = {$inc: {likes: 1}, $push: {ips: req.ip}}
+            next(stockName, docUpdate, priceStock)
+          }
+        })        
       }
 
       // Price of stock
       let priceStock = (stockDocument, next) => {
-        next(stockDocument, outputRes)
+        let httpReq = new httpRequest()
+        let url = 'https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/'+ stockDocument['name'].toUpperCase()+'/quote'
+        httpReq.open('GET', url, true)
+        httpReq.onload = () => {
+          let apiRes = JSON.parse(httpReq.responseText)
+          console.log('Loading...')
+          stockDocument['price'] = apiRes['latestPrice']
+          console.log(stockDocument)
+          next(stockDocument, outputRes)
+        }
+        httpReq.send()
       }
 
       // Build response for 1 stock
       let getOneStock = (stockDocument, next) => {
-        response['data']['stock'] = stockDocument['name']
+        console.log('Loading...3')
+        response['data']['stock'] = stockDocument['name'].toUpperCase()
+        response['data']['price'] = stockDocument['price']
+        response['data']['likes'] = stockDocument['likes']
         next()
       }
 
+      let stocks = []
       // Build response for 2 stock
       let getTwoStock = (stockDocument, next) => {
-
+        let newStock = {}
+        newStock['stock'] = stockDocument['name'].toUpperCase()
+        newStock['price'] = stockDocument['price']
+        newStock['likes'] = stockDocument['likes']
+        stocks.push(newStock)
+        if(stocks.length === 2) {
+          stocks[0]['rel_likes'] = stocks[0]['likes'] - stocks[1]['likes']
+          stocks[1]['rel_likes'] = stocks[1]['likes'] - stocks[0]['likes']
+          response['data'] = stocks
+          next()
+        } else {
+          return
+        }
       }
 
       // Handle input
       if (typeof (req.query.stock) === 'string'){
         // Process with one stock
-        let stock = req.query.stock
-        let documentUpdate = {}
-        findOrUpdateStock(stock, documentUpdate, priceStock)
+        let stock = req.query.stock.toUpperCase()
 
+        if(req.query.like && req.query.like == 'true'){
+          likeStock(stock, findOrUpdateStock)
+        } else {
+          let documentUpdate = {}
+          findOrUpdateStock(stock, documentUpdate, priceStock)
+        }
       } else if (Array.isArray(req.query.stock)){
         isTwoStock = true
         // Process stock 1
-
+        let firstStock = req.query.stock[0].toUpperCase()
+        if(req.query.like && req.query.like == 'true'){
+          likeStock(firstStock, findOrUpdateStock)
+        } else {
+          let documentUpdate = {}
+          findOrUpdateStock(firstStock, documentUpdate, priceStock)
+        }
         // Process stock 2
+        let secondStock = req.query.stock[1].toUpperCase()
+        if(req.query.like && req.query.like == 'true'){
+          likeStock(secondStock, findOrUpdateStock)
+        } else {
+          let documentUpdate = {}
+          findOrUpdateStock(secondStock, documentUpdate, priceStock)
+        }
       }
-    });
-    
+    });    
 };
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -2,9 +2,90 @@ const chaiHttp = require('chai-http');
 const chai = require('chai');
 const assert = chai.assert;
 const server = require('../server');
+let mongoose = require('mongoose')
 
 chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
-    
+    test('1 stock', function(done){
+        chai.request(server)
+            .get('/api/stock-prices')
+            .query({stock: 'goog'})
+            .end((err, res) => {
+                assert.equal(res.body['data']['stock'], 'goog'.toUpperCase())
+                assert.isNotNull(res.body['data']['price'])
+                assert.isNotNull(res.body['data']['likes'])
+                done()
+            })
+    })
+
+    test('1 stock with like', (done) => {
+        chai.request(server)
+            .get('/api/stock-prices')
+            .query({stock: 'aapl', like: true})
+            .end((err, res) => {
+                assert.equal(res.body['data']['stock'], 'aapl'.toUpperCase())
+                assert.equal(res.body['data']['likes'], 1)
+                console.log('One stock with like')
+                console.log(res.body)
+                done()
+            })
+    })
+    test('1 stock with like again ensure likes do not double counted', (done) => {
+        chai.request(server)
+            .get('/api/stock-prices')
+            .query({stock: 'aapl', like: true})
+            .end((err, res) => {
+                assert.equal(res.text, 'Error: Only 1 like per IP allowed')
+                done()
+            })
+    })
+    test('2 stocks', (done) => {
+        chai.request(server)
+            .get('/api/stock-prices')
+            .query({stock: ['aapl', 'amzn']})
+            .end((err, res) => {
+                let stockData = res.body['data']
+                if(stockData[0]['stock'] === 'aapl'.toUpperCase()) {
+                    assert.equal(stockData[0]['stock'], 'aapl'.toUpperCase())
+                    assert.equal(stockData[0]['likes'], 1)
+                    assert.equal(stockData[0]['rel_likes'], 1)
+                    assert.equal(stockData[1]['stock'], 'amzn'.toUpperCase())
+                    assert.equal(stockData[1]['likes'], 0)
+                    assert.equal(stockData[1]['rel_likes'], -1)
+                } else {
+                    assert.equal(stockData[1]['stock'], 'aapl'.toUpperCase())
+                    assert.equal(stockData[1]['likes'], 1)
+                    assert.equal(stockData[1]['rel_likes'], 1)
+                    assert.equal(stockData[0]['stock'], 'amzn'.toUpperCase())
+                    assert.equal(stockData[0]['likes'], 0)
+                    assert.equal(stockData[0]['rel_likes'], -1)
+                }
+                done()
+            })
+    })
+    test('2 stock with like', (done) => {
+        chai.request(server)
+            .get('/api/stock-prices')
+            .query({stock: ['spot', 'amzn'], like: true})
+            .end((err, res) => {
+                let stockData = res.body['data']
+                if(stockData[0]['stock'] === 'spot'.toUpperCase()) {
+                    assert.equal(stockData[0]['stock'], 'spot'.toUpperCase())
+                    assert.equal(stockData[0]['likes'], 1)
+                    assert.equal(stockData[0]['rel_likes'], 0)
+                    assert.equal(stockData[1]['stock'], 'amzn'.toUpperCase())
+                    assert.equal(stockData[1]['likes'], 1)
+                    assert.equal(stockData[1]['rel_likes'], 0)
+                } else {
+                    assert.equal(stockData[1]['stock'], 'spot'.toUpperCase())
+                    assert.equal(stockData[1]['likes'], 1)
+                    assert.equal(stockData[1]['rel_likes'], 0)
+                    assert.equal(stockData[0]['stock'], 'amzn'.toUpperCase())
+                    assert.equal(stockData[0]['likes'], 1)
+                    assert.equal(stockData[0]['rel_likes'], 0)
+                }
+                done()
+            })
+    })
 });
